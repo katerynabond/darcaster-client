27640dbe5f66d5a094651bf928c1550f5d49515e
Kateryna Bondareva <kbondareva@gmail.com>
2017-07-06 19:16:23 -0400

    added webpack-html-plugin in order to automate the creation of the index.html in the dist folder.This allows webpack to manage all of the build processes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
 
+const  HtmlWebpackPlugin = require('html-webpack-plugin');
+
 /*
  * We've enabled UglifyJSPlugin for you! This minifies your app
  * in order to load faster and run less javascript.
@@ -59,5 +61,11 @@ module.exports = {
 		]
 	},
 
-	plugins: [new UglifyJSPlugin(), new ExtractTextPlugin('style.css')]
+	plugins: [new UglifyJSPlugin(),
+						new ExtractTextPlugin('style.css'),
+					  new HtmlWebpackPlugin({
+							title: 'Darcaster',
+							filename: 'index.html',
+							template: './src/index.html'
+						})]
 };
