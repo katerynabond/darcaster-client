945fe41374376747840aafec8b474b50f6784459
Kateryna Bondareva <kbondareva@gmail.com>
2017-07-05 20:57:24 -0400

    our first angular app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+const angular = require('angular');
 
+//create our application
+const app = angular.module('darcaster-client', []); //setter syntax
+
+//
+app.controller('MainController', MainController);
+
+MainController.$inject = ['$scope'];
+
+function MainController($scope){
+  $scope.message = 'hello from angular';
+}
