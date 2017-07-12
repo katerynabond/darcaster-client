4bdb06b40a96d03f839e24c05126391d57fb3a94
Kateryna Bondareva <kbondareva@gmail.com>
2017-07-12 18:58:31 -0400

    added image lookup and a clear day image

--- src/components/current-weather/current-weather.controller.js
+++ src/components/current-weather/current-weather.controller.js
@@ -1,8 +1,13 @@
+const clearDay = require('../../images/clear-day.png');
 MainController.$inject = ['WeatherService'];
 
 function MainController(weather){
   this.message = 'hello from angular';
+  this.imageLookup = {
+    'clear-day': clearDay
+  }
   this.weatherData = weather.getCurrentWeather();
+
 }
 
 module.exports = MainController;
