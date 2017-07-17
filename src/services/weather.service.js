4bd5601677f5ccb71af5451147756ddf429a9b2a
Kateryna Bondareva <kbondareva@gmail.com>
2017-07-16 21:47:06 -0400

    Getting weather by location

--- src/services/weather.service.js
+++ src/services/weather.service.js
@@ -5,15 +5,27 @@ function WeatherService($http){
   const baseUrl = 'https://sheltered-badlands-87459.herokuapp.com/weather/';
   return{
     //label    function name
-    getCurrentWeather: getCurrently
-  }
+    getCurrentWeather: getCurrently,
+    getCurrentWeatherByLocation: getCurrentlyByLocation
+
+  };
 function getCurrently(lat, lon){
   const url = `${baseUrl}${lat},${lon}`;
   return $http.get(url)
               .then(response => {
                 return response.data.currently;
-              })
+              });
 }
+
+function getCurrentlyByLocation(location){
+  const url = `${baseUrl}location/${location}`;
+  return $http.get(url)
+              .then(response => {
+                return response.data.currently;
+              });
+}
+
+
 
 }
 
